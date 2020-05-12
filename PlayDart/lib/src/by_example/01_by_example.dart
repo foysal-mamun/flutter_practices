64149d7ce7766033be50fefdfd72f32aa8936218
Foysal Mamun <sal_mdn@yahoo.com>
2020-05-13 04:58:30 +0600

    practice more dart

--- PlayDart/lib/src/by_example/01_by_example.dart
+++ PlayDart/lib/src/by_example/01_by_example.dart
@@ -28,6 +28,121 @@ class GridContainer extends StatelessWidget {
         children: <Widget>[
 
           customRaisedButton( context: context, label: 'Hello World', callback: () => print('Hello World!!!') ),
+          
+          customRaisedButton( context: context, label: 'Value', callback: () {
+            print("dart" + " Lang");
+            print("1+1=${1+1}");
+            print("0.7/0.3=${0.7/0.3}");
+            print(true && false);
+            print(false || true);
+            print(!true);
+          }),
+
+          customRaisedButton(context: context, label: 'Variables', callback: () {
+            var a = "initial";
+            print(a);
+
+            num i = 10;
+            print(i);
+
+            final num c = 99;
+            print(c);
+            //c = 10;
+
+            const double d = 44.00;
+            print(d);
+            //d = 10;
+          }),
+
+          customRaisedButton(context: context, label: 'For Loop', callback: (){
+            print('C type for loop');
+            for (var i = 0; i < 3; i++) {
+              print(i);
+            }
+
+            print('For IN');
+            var c = [1,2,3];
+            for (var i in c) {
+              print(i);
+            }
+
+            print('for each');
+            var cbs = [];
+            for (var i = 6; i < 8; i++) {
+              cbs.add(() => print(i));
+            }
+            cbs.forEach((c) => c());
+          }),
+
+          customRaisedButton(context: context, label: 'If/Else', callback: () {
+
+            if(7%2 != 0) {
+              print('Odd');
+            } else {
+              print('Even');
+            }
+
+            var isAlive = true;
+            var monday = isAlive ? 'doctor' : null;
+            print('Monday Appointment: $monday');
+
+          }),
+
+          customRaisedButton(context: context, label: 'Null Aware Opt', callback: () {
+
+              var monday = 'doctor';
+              var tuesday;
+              var next = tuesday ?? monday;
+              print('Next Appointment: $next');
+
+              next = null;
+              var wendnesday = 'xyz';
+              next ??= wendnesday;
+              print('Next Appointment: $next');
+
+              String thursday = 'abc';
+              var length = thursday?.length;
+              print('length: $length');
+          }),
+
+          customRaisedButton(context: context, label: 'While/do while', callback: () {
+
+            var i = 0;
+            while(i++ < 2) {
+              print('while $i');
+            }
+
+            var j = 5;
+            do {
+              print('dowhile $j');
+            } while( j-- > 1);
+
+          }),
+
+          customRaisedButton(context: context, label: 'Switch', callback: () {
+
+            var piece = 'knight';
+            switch(piece) {
+              case 'bishop':
+                print('diagonal');
+                break;
+              case 'knight':
+                print('L-shape');
+                break;
+              default:
+                print('checkmate');
+            }
+
+            piece = 'queen';
+            switch(piece) {
+              case 'queen':
+              case 'bishop':
+                print('diagonal');
+                break;
+            }
+
+          }),
+
         ]
       )
     );
