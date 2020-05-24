88e1d4a56fc773ab5324c8c97b3a397a413f2e25
Foysal Mamun <sal_mdn@yahoo.com>
2020-05-24 14:36:24 +0600

    flutter layout

--- layout_in_flutter/lib/main.dart
+++ layout_in_flutter/lib/main.dart
@@ -82,26 +82,210 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: ListView(
-        children: [
-          Image.asset(
-            'images/lake.jpg',
-            width: 600,
-            height: 240,
-            fit: BoxFit.cover
-          ),
-          titleSection,
-          buttonSection,
-          textSection,
-          Divider(height: 1),
-          layout1,
-          Divider(),
-          layout2,
-          Divider()
-        ]
-      )
+      body: 
+        //ListView(
+        //  children: [
+        //    Image.asset(
+        //      'images/lake.jpg',
+        //      width: 600,
+        //      height: 240,
+        //      fit: BoxFit.cover
+        //    ),
+        //    titleSection,
+        //    buttonSection,
+        //    textSection,
+        //    Divider(height: 1),
+        //    layout1,
+        //    Divider(),
+        //    layout2,
+        //    Divider(),
+        //  ]
+        //)
+        //_buildStack(),
+        //_buildList(),
+        //_buildCard(),
+        _buildCardStack()
     );
   }
+
+  Widget _buildCardStack() => SizedBox(
+    height: 350,
+    width: 300,
+    child: Card(
+      child: Column(
+        children: [
+          Stack(
+            alignment: const Alignment(0.1, 0.6),
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images/lake.jpg'),
+                radius: 100
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black45
+                ),
+                child: Text(
+                  'Foysal',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                  )
+                )
+              )
+            ]
+          ),
+          ListTile(
+            title: Text('Number 10'),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Whitehaven Beach'),
+                Text('Whitesunday Island, Whitesunday Beach'),
+                Text('Whitesunday Island, Whitesunday Beach')
+              ]
+            ),
+            isThreeLine: true
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector( 
+                    child: Text(
+                      'Share', 
+                      style: TextStyle( 
+                        decoration: TextDecoration.underline, 
+                        color: Colors.blue[500]
+                      )
+                    ) 
+                  )
+                ),
+                Expanded(
+                  child: GestureDetector( 
+                    child: Text(
+                      'Explore', 
+                      style: TextStyle( 
+                        decoration: TextDecoration.underline, 
+                        color: Colors.blue[500]
+                      )
+                    ) 
+                  )
+                )
+              ]
+            )
+          )
+        ]
+      )
+    )
+  );
+
+  Widget _buildCard() => SizedBox(
+    height: 210,
+    child: Card(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              '1625 Main Street',
+              style: TextStyle(
+                fontWeight: FontWeight.w500
+              )
+            ),
+            subtitle: Text('My City, CA 99984'),
+            leading: Icon(
+              Icons.restaurant_menu,
+              color: Colors.blue[500]
+            )
+          ),
+          Divider(),
+          ListTile(
+            title: Text(
+              '(408) 555-1212',
+              style: TextStyle(
+                fontWeight: FontWeight.w500
+              )
+            ),
+            leading: Icon(
+              Icons.contact_phone,
+              color: Colors.blue[500]
+            )
+          ),
+          ListTile(
+            title: Text(
+              'costa@example.com'
+            ),
+            leading: Icon(
+              Icons.contact_mail,
+              color: Colors.blue[500]
+            )
+          )
+        ]
+      )
+    )
+  );
+
+  Widget _buildList() => ListView(
+    children: [
+      _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+      _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+      _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+      _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+      _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+          Icons.theaters),
+      _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+      Divider(),
+      _tile('Kescaped_code#39;s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+      _tile('Emmyescaped_code#39;s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+      _tile(
+          'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+      _tile('La Ciccia', '291 30th St', Icons.restaurant),
+    ]
+  );
+
+  ListTile _tile( String title, String subtitle, IconData icon ) => ListTile(
+    title: Text(
+      title,
+      style: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 20
+      )
+    ),
+    subtitle: Text(subtitle),
+    leading: Icon(
+      icon,
+      color: Colors.blue[500]
+    )
+  );
+
+
+  Widget _buildStack() => Container(
+    padding: const EdgeInsets.all(32),
+    child: Stack(
+      alignment: const Alignment(0.6, 0.6),
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage('images/lake.jpg'),
+          radius: 100
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.black45
+          ),
+          child: Text(
+            'Foysal Mamun',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+            )
+          )
+        )
+      ]
+    )
+  );
 
   
 
